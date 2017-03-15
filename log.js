dabff4d12d6fa5d21f85b1db07a9fc3bef029745
indexzero <charlie.robbins@gmail.com>
2017-03-14 17:56:14 -0700

    [refactor fix] Update tests to align with current `winston@3` APIs.

--- log.js
+++ log.js
@@ -1,63 +1,60 @@
 var assert = require('assert');
 
-module.exports = function (name, Transport, options) {
+module.exports = function (options) {
   options = options || {};
-  var transport;
-  var logger;
+
+  var Transport = options.Transport;
+  var name = Transport.name || options.name;
+  var construct = options.construct;
+  var instance;
 
   beforeEach(function () {
-    transport = new Transport(options.construct);
-    logger = new options.Logger({ transports: [transport] });
+    instance = new Transport(construct);
   });
 
   // TODO How to assert that error event is thrown? How to make a transport fail?
-  describe('transport log() method', function () {
+  describe('.log()', function () {
     it('should be present', function () {
-      assert.ok(transport.log);
-      assert.equal('function', typeof transport.log);
+      assert.ok(instance.log);
+      assert.equal('function', typeof instance.log);
     });
 
-    describe('logger', function () {
-      describe('.transports', function () {
-        it('should contain transport ' + name, function () {
-          var instance = logger.transports.filter(function (item) {
-            return item === transport;
-          })[0];
+    it('(with no callback) should return true', function () {
+      var info = {
+        level: 'debug',
+        message: 'foo'
+      };
 
-          assert.ok(instance);
-          assert.equal(transport, instance);
-        });
-      });
-
-      describe('log() method', function () {
-        it('should return true', function () {
-          var result = logger.log('debug', 'foo');
-          assert(true, result);
-        });
-      });
+      info.raw = JSON.stringify(info);
+      var result = instance.log(info);
+      assert(true, result);
     });
 
-    describe('events', function () {
-      it.skip('should emit the "logging" event', function (done) {
-        logger.once('logging', function (level, msg, meta) {
-          assert.equal('debug', level);
-          assert.equal('hello', msg);
-          done();
-        });
+    it('(with callback) should return true', function (done) {
+      var info = {
+        level: 'debug',
+        message: 'foo'
+      };
 
-        logger.log('debug', 'hello');
+      info.raw = JSON.stringify(info);
+      var result = instance.log(info, function () {
+        assert(true, result);
+        done();
       });
+    });
+  });
 
-      it.skip('should emit the "logged" event', function (done) {
-        transport.once('logged', function (err, level, msg, meta) {
-          assert.ifError(err);
-          assert.equal('debug', level);
-          assert.equal('hello', msg);
-          done();
-        });
+  describe('events', function () {
+    it('should emit the "logged" event', function (done) {
+      instance.once('logged', done);
 
-        logger.log('debug', 'hello');
-      });
+      var info = {
+        level: 'debug',
+        message: 'foo'
+      };
+
+      info.raw = JSON.stringify(info);
+      instance.log(info);
     });
   });
 };
