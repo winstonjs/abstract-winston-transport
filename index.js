c26de015fe8b721c81c52d965aee4d9c3a8bfb71
indexzero <charlie.robbins@gmail.com>
2017-03-19 13:36:40 -0400

    [doc] Update JSDoc for `module.exports`.

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 /**
- * Winston Transport Abstract Test Suite
+ * Winston TransportStream Abstract Test Suite
  *
- * @method
- * @static
- * @param {String}            name      Transport name
- * @param {Function}          Transport Class to instantiate (new Transport)
- * @param {Object|Function}   options   Options to pass to the transport to instantiate.
- *                                      If passed an object, it will be passed as is when
- *                                      calling `new Transport(options)`. If passed a
- *                                      function it will be called before:
- *                                      `new Transport(options())`.
- *                                      If parameter left empty defaults to {}.
+ * @param {Object} opts Options for your test suite execution
+ *   @param {String}          opts.name      Transport name.
+ *   @param {TransportStream} opts.Transport Class to instantiate (new Transport).
+ *   @param {Boolean}         opts.query     If true, will also include "Query" suite.
+ *   @param {Boolean}         opts.stream    If true, will also include "Stream" suite.
+ *   @param {Object}          opts.construct Options to pass to the transport to instantiate.
+ *      - If passed an object, it will be passed as is when calling `new Transport(options)`.
+ *      - If passed a function it will be called before: `new Transport(options())`.
+ *      - If parameter left empty defaults to {}.
  */
 module.exports = function (options) {
   if (!options || !options.name || !options.Transport) {
